b28354c6ab3a0a39c5cd9486656175099301b2c3
Ben Huang <benxu.huang@gmail.com>
2021-03-30 14:05:17 +0800

    add 0089._Gray_Code__Medium

---
+++ 0089. Gray Code (Medium)/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _0089._Gray_Code__Medium_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var res = grayCode(4);
+        }
+
+        static List<int> grayCode(int n)
+        {
+            List<int> res = new List<int>();
+
+            res.Add(0);
+
+            if (n > 0)
+            {
+                int adder = 1;
+
+                for (int i = 1; i <= n; i++)
+                {
+                    int len = res.Count;
+
+                    for (int j = len - 1; j >= 0; j--)
+                    {
+                        res.Add(res[j] + adder);
+                    }
+
+                    adder *= 2;
+                }
+            }
+            return res;
+        }
+    }
+}
