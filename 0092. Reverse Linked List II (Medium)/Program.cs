866b3d928a27127acf3b849f2ea994bd1a8ba653
Ben Huang <benxu.huang@gmail.com>
2021-04-06 13:04:54 +0800

    0098._Validate_Binary_Search_Tree__Medium

---
+++ 0092. Reverse Linked List II (Medium)/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _0092._Reverse_Linked_List_II__Medium_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListNode node = new ListNode(1);
+            node.next = new ListNode(2);
+            node.next.next = new ListNode(3);
+            node.next.next.next = new ListNode(4);
+            node.next.next.next.next = new ListNode(5);
+            var result = ReverseBetween(node, 3, 4);
+        }
+
+        public static ListNode ReverseBetween(ListNode head, int left, int right)
+        {
+            if (left == right) return head;
+
+            Stack<ListNode> st = new Stack<ListNode>();
+            ListNode leftNode = null;
+            ListNode node = head;
+
+            if (left != 1)
+            {
+                for (int i = 1; i < left - 1; ++i) 
+                    node = node.next;
+
+                leftNode = node;
+                node = node.next; // position m
+            }
+
+            for (int i = left; i <= right; ++i)
+            {
+                st.Push(node);
+                node = node.next;
+            }
+
+            ListNode l1 = st.Pop(), l2 = null;
+
+            if (left == 1)
+                head = l1;
+            else
+                leftNode.next = l1;
+
+            while (st.Count != 0)
+            {
+                l2 = st.Pop();
+                l1.next = l2;
+                l1 = l2;
+            }
+            l1.next = node;
+
+            return head;
+        }
+
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next = null;
+            public ListNode(int x) { val = x; }
+        }
+    }
+}
---
+++ 0098. Validate Binary Search Tree (Medium)/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _0098._Validate_Binary_Search_Tree__Medium_
+{
+    class Program
+    {
+        static TreeNode last;
+
+        static void Main(string[] args)
+        {
+            TreeNode root = new TreeNode(43);
+            root.left = new TreeNode(33);
+            root.right = new TreeNode(61);
+            root.left.left = new TreeNode(20);
+            root.left.right = new TreeNode(37);
+            root.right.left = new TreeNode(54);
+            root.right.right = new TreeNode(66);
+
+            Console.WriteLine(isValidBST(root));
+        }
+
+        static bool isValidBST(TreeNode root)
+        {
+            if (root == null) return true;
+            if (!isValidBST(root.left)) return false;
+            if (last != null && root.val <= last.val) return false;
+            last = root;
+            return isValidBST(root.right);
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+
+}
