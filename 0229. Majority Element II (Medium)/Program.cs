77a6eb3d02d5cee736027783e7fe24269d60e6ad
Ben Huang <benxu.huang@gmail.com>
2021-04-16 16:31:25 +0800

    0229.Majority_Element_II__Medium_

---
+++ 0229. Majority Element II (Medium)/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _0229.Majority_Element_II__Medium_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 1, 2, 2, 1, 1, 1, 1, 2, 2, 1, 1 };
+            Console.WriteLine(MajorityElement(nums));
+        }
+
+        public static IList<int> MajorityElement(int[] nums)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (!dic.ContainsKey(nums[i]))
+                    dic.Add(nums[i], 1);
+                else
+                    dic[nums[i]] = dic[nums[i]] + 1;
+            }
+
+            int times = nums.Length / 3;
+
+            return dic.Where(x => x.Value > times).Select(x => x.Key).ToList();
+        }
+    }
+}
