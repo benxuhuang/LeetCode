d3b0eac20cf96ab26246e211c948f21ee3f3cfb5
Ben Huang <benxu.huang@gmail.com>
2021-04-23 14:55:27 +0800

    0096.Unique_Binary_Search_Trees__Medium_

---
+++ 0096. Unique Binary Search Trees (Medium)/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _0096.Unique_Binary_Search_Trees__Medium_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NumTrees(5));
+        }
+
+
+        public static int NumTrees(int n)
+        {
+            int[] dp = new int[n + 1];
+            dp[0] = 1;
+
+            for (int i = 0; i <= n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    dp[i] += dp[j] * dp[i - j - 1];
+                }
+            }
+
+            return dp[n];
+        }
+    }
+}
