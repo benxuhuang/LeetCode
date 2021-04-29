6e17cfec232df5796c89e9308ce169fe8bbb5575
Ben Huang <benxu.huang@gmail.com>
2021-04-29 15:50:05 +0800

    _0144.Binary_Tree_Preorder_Traversal__Medium_

---
+++ 0144. Binary Tree Preorder Traversal (Medium)/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _0144.Binary_Tree_Preorder_Traversal__Medium_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeNode root = new TreeNode(1);
+            root.right = new TreeNode(5);
+            root.right.left = new TreeNode(7);
+            root.right.right = new TreeNode(8);
+            root.left = new TreeNode(3);
+            root.left.left = new TreeNode(2);
+            root.left.right = new TreeNode(3);
+            Console.WriteLine(PreOrderTraversalByIterative(root));
+
+            List<int> result = new List<int>();
+            PreOrderTraversalByRecursive(root, result);
+            Console.WriteLine(result.ToString());
+        }
+
+
+        //preorder: root -> left -> right
+        public static List<int> PreOrderTraversalByIterative(TreeNode root)
+        {
+            List<int> result = new List<int>();
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+
+            if (root != null)
+            {
+                stack.Push(root);
+                while (stack.Count != 0)
+                {
+                    TreeNode n = stack.Pop();
+                    if (n.right != null)
+                        stack.Push(n.right);
+                    if (n.left != null)
+                        stack.Push(n.left);
+                    result.Add(n.val);
+                }
+            }
+            return result;
+        }
+
+        public static void PreOrderTraversalByRecursive(TreeNode root, List<int> result)
+        {
+            if (root == null) return;
+            result.Add(root.val);
+            PreOrderTraversalByRecursive(root.left, result);
+            PreOrderTraversalByRecursive(root.right, result);
+        }
+
+
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x) { val = x; }
+        }
+
+    }
+}
---
+++ 0213. House Robber II (Medium)/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _0213.House_Robber_II__Medium_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[] { 1, 3, 2, 5, 6, 9, 4, 3, 9 };
+            Console.WriteLine(Rob(num));
+        }
+
+        public static int Rob(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+            if (nums.Length == 1) return nums[0];
+            int l = nums.Length;
+            int res, prev, curr;
+
+            // rob house 0 -> can't rob house l-1 -> result = dp[l-2]
+            prev = nums[0];
+            curr = nums[0];
+            for (int i = 2; i < l - 1; ++i)
+            {
+                int tmp = prev;
+                prev = curr;
+                curr = Math.Max(prev, tmp + nums[i]);
+            }
+            res = curr;
+
+            // don't rob house 0 -> result = dp[l-1]
+            prev = 0;
+            curr = nums[1];
+            for (int i = 2; i < l; ++i)
+            {
+                int tmp = prev;
+                prev = curr;
+                curr = Math.Max(prev, tmp + nums[i]);
+            }
+            res = Math.Max(res, curr);
+
+            return res;
+        }
+    }
+}
