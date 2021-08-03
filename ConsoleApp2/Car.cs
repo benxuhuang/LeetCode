048197c4bd218f516a688478c06635f7e1607295
Ben Huang <benxu.huang@gmail.com>
2021-08-03 11:16:48 +0800

    update

---
+++ ConsoleApp2/Car.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp2
+{
+    public class Car
+    {
+        public string Brand { get; set; }
+        public int Price { get; set; }
+
+        public Car(string brand, int price)
+        {
+            Brand = brand;
+            Price = price;
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int?[] vals = { 1, 2, 3, null, 4, 5 };
+
+            int i = 0;
+
+            while (i < vals.Length)
+            {
+                Console.WriteLine(vals[i].GetType());
+                i++;
+            }
+
+            Console.ReadLine();
+        }
+
+        class User
+        {
+            public User() { }
+
+            public User(string name, string occupation)
+            {
+                this.name = name;
+                this.occupation = occupation;
+            }
+
+            public string name { get; set; }
+            public string occupation { get; set; }
+
+            public override string ToString() => $"{name} {occupation}";
+        }
+
+
+    }
+}
